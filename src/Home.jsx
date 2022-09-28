189215b9c85dd43a775696d1d8a1bae2db08c252
Bienvenido Jamon III <108235250+bienjam@users.noreply.github.com>
2022-09-29 01:28:41 +0800

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Veggie from "./Veggie"
+import Popular from "./Popular"
+
+
+
+function Home() {
+  return (
+    <div>
+        <Popular />
+        <Veggie />
+        
+    </div>
+  )
+}
+
+export default Home
